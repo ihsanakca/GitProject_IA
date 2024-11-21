72cbb83a6025b1b4c946694e7f2912d341d43756
ihsan akcair <ihsanakcair@gmail.com>
2024-11-21 20:50:25 +0300

    AAA python ve c++ eklendi

--- src/AAA.java
+++ src/AAA.java
@@ -1,5 +1,7 @@
 public class AAA {
     public static void main(String[] args) {
         System.out.println("Java");
+        System.out.println("Python");
+        System.out.println("C++");
     }
 }
